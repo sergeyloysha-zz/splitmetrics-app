25ccd808d71b3aec5a977869fed989aea530754e
Sergey Loysha <sergeyloysha@gmail.com>
2015-10-19 16:40:02 +0300

    Update 2015-10-19T13:39:57.622Z

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+/*
+ * SplitMetrics App v1.0
+ * https://github.com/sergeyloysha/splitmetrics-app
+ * Copyright (C) 2015 Sergey Loysha <sergeyloysha@gmail.com>
+ * https://github.com/sergeyloysha/splitmetrics-app/blob/master/LICENSE
+ */
+
+'use strict';
+
+angular.module('SplitMetrics', [
+  'angular-loading-bar',
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers',
+])
+
+.config(['$routeProvider', 'cfpLoadingBarProvider', function($routeProvider, cfpLoadingBarProvider) {
+
+}])
+
+.constant('config', {
+
+})
+angular.module('SplitMetrics.controllers', [])
+angular.module('SplitMetrics.directives', [])
+angular.module('SplitMetrics.services', [])
+angular.module('SplitMetrics.filters', [])
